26b7dd4d3da455841bcd8e9812223a9fe7aa6927
bjvanlinschoten <bjvanlinschoten@gmail.com>
2015-06-02 22:46:59 +0200

    Added Xcode project

---
+++ EventDateApp/EventDateApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EventDateApp
+//
+//  Created by Boris van Linschoten on 02-06-15.
+//  Copyright (c) 2015 bjvanlinschoten. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
