bf0ce5fd20f8cbcbc8a2e7c1ad1dd346a41de64c
bjvanlinschoten <bjvanlinschoten@gmail.com>
2015-06-02 23:56:25 +0200

    Start on including the Facebook SDK (not working yet)

---
+++ EventDateApp/EventDateApp/Bridge-Header.h
@@ -0,0 +1,16 @@
+//
+//  Bridge-Header.h
+//  EventDateApp
+//
+//  Created by Boris van Linschoten on 02-06-15.
+//  Copyright (c) 2015 bjvanlinschoten. All rights reserved.
+//
+
+#ifndef EventDateApp_Bridge_Header_h
+#define EventDateApp_Bridge_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+
+#endif
